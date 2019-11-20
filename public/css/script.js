08df706ab027db31a1b6801286607d1d1c73a8c2
marj <ahmet.guldas@hotmail.com>
2019-11-20 16:12:15 +0200

    tema dosyaları entegre işlemine başlandı

---
+++ public/css/script.js
@@ -0,0 +1,1 @@
+jQuery('#menu').metisMenu().show();
---
+++ resources/views/layouts/index.blade.php
@@ -0,0 +1,158 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
+    <title>@yield('title', 'başlık')</title>
+    @include('.partials.style')
+</head>
+
+<body>
+    <nav class="navbar navbar-dark navbar-expand-lg fixed-top bg-white portfolio-navbar gradient">
+        <div class="container"><button data-toggle="collapse" class="navbar-toggler" data-target="#navbarNav"><span class="sr-only">Toggle navigation</span></button>
+            <div class="collapse navbar-collapse" id="navbarNav"><a href="#">2020 PDF KATALOĞUMUZU İNCELEMEK İÇİN TIKLAYINIZ.</a>
+                <ul class="nav navbar-nav ml-auto">
+                    <li class="nav-item" role="presentation"><a class="nav-link active" href="index.html">info@marjpromosyon.com<i class="icon ion-android-mail"></i></a></li>
+                    <li class="nav-item" role="presentation"></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+    <main id="navbarNav" class="page lanidng-page">
+        <nav class="navbar navbar-light navbar-expand-md navigation-clean-search fixed-top">
+            <div class="container"><a class="navbar-brand" href="#">MarjPromosyon</a><button data-toggle="collapse" class="navbar-toggler" data-target="#navcol-1"><span class="sr-only">Toggle navigation</span><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse"
+                    id="navcol-1">
+                    <ul class="nav navbar-nav">
+                        <li class="nav-item" role="presentation"><a class="nav-link active" href="#">Link 1</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Link 2</a></li>
+                        <li class="nav-item" role="presentation"><a class="nav-link" href="#">Link 3</a></li>
+                    </ul>
+                    <form class="form-inline mr-auto" target="_self">
+                        <div class="form-group"><label for="search-field"><i class="fa fa-search"></i></label></div>
+                    </form><a class="d-lg-flex align-items-lg-center" data-aos="zoom-in" data-aos-duration="2000" href="#"><i class="fa fa-whatsapp" data-bs-parallax-bg="true"></i>&nbsp;0555 555 55 65</a><a class="d-lg-flex align-items-lg-center" data-aos="zoom-in"
+                        data-aos-duration="2000" href="#"><i class="fa fa-phone"></i>&nbsp;0312 418 46 00</a><a class="d-lg-flex align-items-lg-center" data-aos="zoom-in" data-aos-duration="2000" href="#"><i class="fa fa-phone"></i>&nbsp;0312 418 46 00</a></div>
+            </div>
+        </nav>
+    </main>
+    <div class="simple-slider">
+        <div class="swiper-container">
+            <div class="swiper-wrapper">
+                <div class="swiper-slide" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=68&amp;txt=Slideshow+Image&amp;w=1920&amp;h=500);"></div>
+                <div class="swiper-slide" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=68&amp;txt=Slideshow+Image&amp;w=1920&amp;h=500);"></div>
+                <div class="swiper-slide" style="background-image:url(https://placeholdit.imgix.net/~text?txtsize=68&amp;txt=Slideshow+Image&amp;w=1920&amp;h=500);"></div>
+            </div>
+            <div class="swiper-pagination"></div>
+            <div class="swiper-button-prev"></div>
+            <div class="swiper-button-next"></div>
+        </div>
+    </div>
+    <div class="features-boxed">
+        <div class="container">
+            <div class="intro"></div>
+            <section class="portfolio-block projects-cards">
+                <div class="container">
+                    <div class="heading">
+                        <h2>pROMOSYON ÜRÜNLERİ</h2>
+                    </div>
+                    <div class="row">
+                        <div class="col-md-6 col-lg-4">
+                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(7).png" alt="Card Image"></a>
+                                <div class="card-body">
+                                    <h6><a href="#">Lorem Ipsum</a></h6>
+                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(8).png" alt="Card Image"></a>
+                                <div class="card-body">
+                                    <h6><a href="#">Lorem Ipsum</a></h6>
+                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(9).png" alt="Card Image"></a>
+                                <div class="card-body">
+                                    <h6><a href="#">Ajandalar</a></h6>
+                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(4).png" alt="Card Image"></a>
+                                <div class="card-body">
+                                    <h6><a href="#">Lorem Ipsum</a></h6>
+                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(5).png" alt="Card Image"></a>
+                                <div class="card-body">
+                                    <h6><a href="#">Lorem Ipsum</a></h6>
+                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-md-6 col-lg-4">
+                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(6).png" alt="Card Image"></a>
+                                <div class="card-body">
+                                    <h6><a href="#">Lorem Ipsum</a></h6>
+                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </div>
+    <div class="footer-clean">
+        <footer>
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Services</h3>
+                        <ul>
+                            <li><a href="#">Web design</a></li>
+                            <li><a href="#">Development</a></li>
+                            <li><a href="#">Hosting</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>About</h3>
+                        <ul>
+                            <li><a href="#">Company</a></li>
+                            <li><a href="#">Team</a></li>
+                            <li><a href="#">Legacy</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-sm-4 col-md-3 item">
+                        <h3>Careers</h3>
+                        <ul>
+                            <li><a href="#">Job openings</a></li>
+                            <li><a href="#">Employee success</a></li>
+                            <li><a href="#">Benefits</a></li>
+                        </ul>
+                    </div>
+                    <div class="col-lg-3 item social"><a href="#"><i class="icon ion-social-facebook"></i></a><a href="#"><i class="icon ion-social-twitter"></i></a><a href="#"><i class="icon ion-social-snapchat"></i></a><a href="#"><i class="icon ion-social-instagram"></i></a>
+                        <p class="copyright">Company Name © 2017</p>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    </div>
+    <script src="js/jquery.min.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    <script src="js/bs-animation.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/aos/2.1.1/aos.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/pikaday/1.6.1/pikaday.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/Swiper/3.3.1/js/swiper.jquery.min.js"></script>
+    <script src="js/Simple-Slider.js"></script>
+    <script src="js/theme.js"></script>
+</body>
+
+</html>
