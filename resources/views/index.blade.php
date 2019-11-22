5c0f83a3c026a1792b8f871a6b0d7a67628497e4
marj <ahmet.guldas@hotmail.com>
2019-11-22 16:48:12 +0200

    anasayfada promosyon ürünleri kısmı düzenlendi tek şablon gösteriliyor.Dinamikleştirilecek

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,54 +15,29 @@
                         <h2>PROMOSYON ÜRÜNLERİ</h2>
                     </div>
                     <div class="row">
-                        <div class="col-md-6 col-lg-4">
-                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(7).png" alt="Card Image"></a>
-                                <div class="card-body">
-                                    <h6><a href="#">Lorem Ipsum</a></h6>
-                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
-                                </div>
-                            </div>
-                        </div>
+
+
+
+
+
+
+
                         <div class="col-md-6 col-lg-4">
                             <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(8).png" alt="Card Image"></a>
                                 <div class="card-body">
-                                    <h6><a href="#">Lorem Ipsum</a></h6>
-                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
+                                    <h6><a href="#">Başlık</a></h6>
+                                    <p class="text-muted card-text">Açıklama</p>
                                 </div>
                             </div>
                         </div>
-                        <div class="col-md-6 col-lg-4">
-                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(9).png" alt="Card Image"></a>
-                                <div class="card-body">
-                                    <h6><a href="#">Ajandalar</a></h6>
-                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(4).png" alt="Card Image"></a>
-                                <div class="card-body">
-                                    <h6><a href="#">Lorem Ipsum</a></h6>
-                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(5).png" alt="Card Image"></a>
-                                <div class="card-body">
-                                    <h6><a href="#">Lorem Ipsum</a></h6>
-                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-md-6 col-lg-4">
-                            <div class="card border-0"><a href="#"><img class="card-img-top scale-on-hover" src="img/AJANDA%20VE%20BLOKNOTLAR%20(6).png" alt="Card Image"></a>
-                                <div class="card-body">
-                                    <h6><a href="#">Lorem Ipsum</a></h6>
-                                    <p class="text-muted card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc quam urna.</p>
-                                </div>
-                            </div>
-                        </div>
+
+
+
+
+
+
+
+
                     </div>
                 </div>
             </section>
